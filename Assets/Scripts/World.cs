8384bca2cee9a4fd1927ebefad8bb3cf57715986
Francesco Cagnin <francesco.cagnin@gmail.com>
2016-02-15 17:28:59 +0100

    added node as prefab

--- Assets/Scripts/World.cs
+++ Assets/Scripts/World.cs
@@ -3,30 +3,22 @@
 
 public class World : MonoBehaviour
 {
-    void Start()
-    {
-        for (float x = -1; x < 2; x += 0.5f)
-        {
-            for (float y = -1; y < 2; y += 0.5f)
-            {
-                GameObject brick = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                brick.transform.localScale = new Vector3(0.02f, 0.02f, 0.02f);
-                brick.transform.position = new Vector3(x, y, 0f);
-                brick.transform.parent = gameObject.transform;
-                brick.AddComponent(System.Type.GetType("Brick"));
-            }
-        }
-
-        for (float z = -3; z < 0; z += 0.5f)
-        {
-            for (float y = -1; y < 2; y += 0.5f)
-            {
-                GameObject brick = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                brick.transform.localScale = new Vector3(0.02f, 0.02f, 0.02f);
-                brick.transform.position = new Vector3(-1f, y, z);
-                brick.transform.parent = gameObject.transform;
-                brick.AddComponent(System.Type.GetType("Brick"));
-            }
-        }
-    }
+	void Start ()
+	{
+		// draw grid of nodes to experiments on (will be removed)
+		for (float x = -1; x < 2; x += 0.5f) {
+			for (float y = -1; y < 2; y += 0.5f) {
+				GameObject node = (GameObject) Instantiate(Resources.Load ("Node"), new Vector3 (x, y, 0f), Quaternion.identity);
+				node.transform.localScale = new Vector3 (0.02f, 0.02f, 0.02f);
+				node.transform.parent = gameObject.transform;
+			}
+		}
+		for (float z = -3; z < 0; z += 0.5f) {
+			for (float y = -1; y < 2; y += 0.5f) {
+				GameObject node = (GameObject) Instantiate(Resources.Load ("Node"), new Vector3 (-1f, y, z), Quaternion.identity);
+				node.transform.localScale = new Vector3 (0.02f, 0.02f, 0.02f);
+				node.transform.parent = gameObject.transform;
+			}
+		}
+	}
 }
