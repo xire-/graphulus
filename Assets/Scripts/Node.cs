b3f9c301f91319b3d2c4a27ba596a84bb48fad2f
Francesco Cagnin <francesco.cagnin@gmail.com>
2016-02-20 11:18:33 +0100

    removed node movement towards camera

--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -3,14 +3,4 @@
 
 public class Node : MonoBehaviour
 {
-    void FixedUpdate()
-    {
-		// simulate node movement to camera (will be removed)
-		var camera = GameObject.Find("Main Camera");
-        if (Vector3.Distance(gameObject.transform.position, camera.transform.position) > 0.5f)
-        {
-            Vector3 direction = camera.transform.position - gameObject.transform.position;
-            gameObject.transform.position += Time.fixedDeltaTime * (0.3f * direction);
-        }
-    }
 }
