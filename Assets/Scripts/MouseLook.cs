ec6152783b25721ab71f5995c73c4bd9775c64a8
Francesco Cagnin <francesco.cagnin@gmail.com>
2016-02-20 11:19:24 +0100

    on mouse left click camera will move to the selected gameobject

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -12,10 +12,16 @@ public class MouseLook : MonoBehaviour
     float rotationY = 0f;
 
     private GameObject curr;
+    private Vector3? moveTo;
 
 
     void Update()
     {
+        if (moveTo.HasValue)
+        {
+            GameObject.Find("Main Camera").transform.position = Vector3.MoveTowards(GameObject.Find("Main Camera").transform.position, moveTo.Value, 0.5f * Time.deltaTime);
+        }
+
         if (!Input.GetKey(KeyCode.Space))
         {
             float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
@@ -28,18 +34,29 @@ public class MouseLook : MonoBehaviour
 
         RaycastHit hit;
         var cameratrans = GameObject.Find("Main Camera").transform;
-        if (Physics.Raycast(cameratrans.position, cameratrans.forward, out hit)) {
+        if (Physics.Raycast(cameratrans.position, cameratrans.forward, out hit))
+        {
             curr = hit.transform.gameObject;
-            curr.GetComponent<Renderer>().material.color = Color.red;
-        } else {
-            if (curr) {
+            if (Input.GetMouseButtonDown(0))
+            {
+                moveTo = curr.transform.position;
+            }
+            else
+            {
+                curr.GetComponent<Renderer>().material.color = Color.red;
+            }
+        }
+        else {
+            if (curr)
+            {
                 curr.GetComponent<Renderer>().material.color = Color.white;
                 curr = null;
             }
         }
     }
 
-    void OnGUI(){
-        GUI.Box(new Rect(Screen.width/2,Screen.height/2, 2, 2), "");
+    void OnGUI()
+    {
+        GUI.Box(new Rect(Screen.width / 2, Screen.height / 2, 2, 2), "");
     }
 }
