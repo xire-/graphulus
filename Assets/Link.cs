9d4016111b15ef4173eff5a3f9f87e5db0866d29
Francesco Cagnin <francesco.cagnin@gmail.com>
2016-05-24 16:21:53 +0200

    added links

---
+++ Assets/Link.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Link : MonoBehaviour {
+    public Node node1, node2;
+
+	void LateUpdate () {
+        float width = 0.003f;
+        var offset = node2.transform.position - node1.transform.position;
+        var scale = new Vector3 (width, offset.magnitude / 2f, width);
+        transform.position = node1.transform.position + (offset / 2f);
+        transform.up = offset;
+        transform.localScale = scale;
+	}
+}
--- Assets/Scripts/World.cs
+++ Assets/Scripts/World.cs
@@ -1,28 +1,55 @@
 ﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class World : MonoBehaviour
 {
+    private Dictionary<string, Node> nodes = new Dictionary<string, Node> ();
+    Dictionary<Node, Vector3> moveto = new Dictionary<Node, Vector3> ();
+
     void Start ()
     {
-        // draw grid of nodes to experiments on (will be removed)
-        for (float x = -1; x < 2; x += 0.5f) {
-            for (float y = -1; y < 2; y += 0.5f) {
-                GameObject o = (GameObject)Instantiate (Resources.Load ("Node"), new Vector3 (x, y, 0f), Quaternion.identity);
-                Node node = o.GetComponent<Node> ();
-                node.transform.localScale = new Vector3 (0.02f, 0.02f, 0.02f);
-                node.transform.parent = gameObject.transform;
-                node.Value = String.Format ("{0:f}/{1:f}/0", x, y);
-            }
+        // load nodes to experiment on (will be removed)
+        string nconf = System.IO.File.ReadAllText ("Examples/nodes.txt");
+        foreach (var line in nconf.Split(new string[] { Environment.NewLine }, StringSplitOptions.None)) {
+            GameObject o = (GameObject)Instantiate (Resources.Load ("Node"));
+            o.transform.position = new Vector3 (UnityEngine.Random.Range (-0.5f, 0.5f), UnityEngine.Random.Range (-0.5f, 0.5f), UnityEngine.Random.Range (-1f, 0f));
+            o.transform.localScale = new Vector3 (0.02f, 0.02f, 0.02f);
+            o.transform.parent = gameObject.transform;
+            Node node = o.GetComponent<Node> ();
+            node.Value = line;
+            nodes.Add (line, node);
+            moveto.Add (node, node.transform.position);
         }
-        for (float z = -3; z < 0; z += 0.5f) {
-            for (float y = -1; y < 2; y += 0.5f) {
-                GameObject o = (GameObject)Instantiate (Resources.Load ("Node"), new Vector3 (-1f, y, z), Quaternion.identity);
-                Node node = o.GetComponent<Node> ();
-                node.transform.localScale = new Vector3 (0.02f, 0.02f, 0.02f);
-                node.transform.parent = gameObject.transform;
-                node.Value = String.Format ("-1/{0:f}/{1:f}", y, z);
-            }
+
+        List<Node> links = new List<Node> ();
+        string lconf = System.IO.File.ReadAllText ("Examples/links.txt");
+        foreach (var line in lconf.Split(new string[] { Environment.NewLine }, StringSplitOptions.None)) {
+            string start = line.Split (' ') [0];
+            string end = line.Split (' ') [1];
+            Node startnode = nodes [start];
+            Node endnode = nodes [end];
+
+            GameObject o = (GameObject)Instantiate (Resources.Load ("Link"));
+            o.transform.parent = gameObject.transform;
+
+            Link link = o.GetComponent<Link> ();
+            link.node1 = startnode;
+            link.node2 = endnode;
         }
+    }
+
+    void Update ()
+    {
+        // randomly move nodes
+        List<Node> keys = new List<Node> (moveto.Keys);
+        foreach (Node node in keys) {
+            var to = moveto [node];
+            if (node.transform.position == to) {
+                moveto [node] = new Vector3 (UnityEngine.Random.Range (-0.5f, 0.5f), UnityEngine.Random.Range (-0.5f, 0.5f), UnityEngine.Random.Range (-1f, 0f));
+            } else {
+                node.transform.position = Vector3.MoveTowards (node.transform.position, to, 0.2f * Time.deltaTime);
+            }
+        }        
     }
 }
