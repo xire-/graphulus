82a0e80ea9e1e0a20629032db05ec956fafca6ba
Francesco Cagnin <fcagnin@kappa.lan>
2016-05-24 11:55:32 +0200

    formatted

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -15,48 +15,40 @@ public class MouseLook : MonoBehaviour
     private Vector3? moveTo;
 
 
-    void Update()
+    void Update ()
     {
-        if (moveTo.HasValue)
-        {
-            GameObject.Find("Main Camera").transform.position = Vector3.MoveTowards(GameObject.Find("Main Camera").transform.position, moveTo.Value, 0.5f * Time.deltaTime);
+        if (moveTo.HasValue) {
+            GameObject.Find ("Main Camera").transform.position = Vector3.MoveTowards (GameObject.Find ("Main Camera").transform.position, moveTo.Value, 0.5f * Time.deltaTime);
         }
 
-        if (!Input.GetKey(KeyCode.Space))
-        {
-            float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
+        if (!Input.GetKey (KeyCode.Space)) {
+            float rotationX = transform.localEulerAngles.y + Input.GetAxis ("Mouse X") * sensitivityX;
 
-            rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
-            rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+            rotationY += Input.GetAxis ("Mouse Y") * sensitivityY;
+            rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
 
-            transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0f);
+            transform.localEulerAngles = new Vector3 (-rotationY, rotationX, 0f);
         }
 
         RaycastHit hit;
-        var cameratrans = GameObject.Find("Main Camera").transform;
-        if (Physics.Raycast(cameratrans.position, cameratrans.forward, out hit))
-        {
+        var cameratrans = GameObject.Find ("Main Camera").transform;
+        if (Physics.Raycast (cameratrans.position, cameratrans.forward, out hit)) {
             curr = hit.transform.gameObject;
-            if (Input.GetMouseButtonDown(0))
-            {
+            if (Input.GetMouseButtonDown (0)) {
                 moveTo = curr.transform.position;
+            } else {
+                curr.GetComponent<Renderer> ().material.color = Color.red;
             }
-            else
-            {
-                curr.GetComponent<Renderer>().material.color = Color.red;
-            }
-        }
-        else {
-            if (curr)
-            {
-                curr.GetComponent<Renderer>().material.color = Color.white;
+        } else {
+            if (curr) {
+                curr.GetComponent<Renderer> ().material.color = Color.white;
                 curr = null;
             }
         }
     }
 
-    void OnGUI()
+    void OnGUI ()
     {
-        GUI.Box(new Rect(Screen.width / 2, Screen.height / 2, 2, 2), "");
+        GUI.Box (new Rect (Screen.width / 2, Screen.height / 2, 2, 2), "");
     }
 }
