3ffb502b0434472eb26b8d965cf2cc9458c931d9
Francesco Cagnin <francesco.cagnin@gmail.com>
2016-05-25 12:37:45 +0200

    reworked Link to use LineRenderer

---
+++ Assets/Scripts/Link.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Link : MonoBehaviour
+{
+    private LineRenderer lineRenderer;
+    public GameObject node1, node2;
+
+    public void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.SetWidth(0.003f, 0.003f);
+    }
+
+    public void LateUpdate()
+    {
+        lineRenderer.SetPosition(0, node1.transform.position);
+        lineRenderer.SetPosition(1, node2.transform.position);
+    }
+}
--- Assets/Scripts/World.cs
+++ Assets/Scripts/World.cs
@@ -4,52 +4,56 @@ using UnityEngine;
 
 public class World : MonoBehaviour
 {
-    private Dictionary<string, Node> nodes = new Dictionary<string, Node> ();
-    Dictionary<Node, Vector3> moveto = new Dictionary<Node, Vector3> ();
+    private Dictionary<string, GameObject> nodes = new Dictionary<string, GameObject>();
+    Dictionary<GameObject, Vector3> moveto = new Dictionary<GameObject, Vector3>();
 
-    void Start ()
+    void Start()
     {
-        // load nodes to experiment on (will be removed)
-        string nconf = System.IO.File.ReadAllText ("Examples/nodes.txt");
-        foreach (var line in nconf.Split(new string[] { Environment.NewLine }, StringSplitOptions.None)) {
-            GameObject o = (GameObject)Instantiate (Resources.Load ("Node"));
-            o.transform.position = new Vector3 (UnityEngine.Random.Range (-0.5f, 0.5f), UnityEngine.Random.Range (-0.5f, 0.5f), UnityEngine.Random.Range (-1f, 0f));
-            o.transform.localScale = new Vector3 (0.02f, 0.02f, 0.02f);
-            o.transform.parent = gameObject.transform;
-            Node node = o.GetComponent<Node> ();
-            node.Value = line;
-            nodes.Add (line, node);
-            moveto.Add (node, node.transform.position);
+        var nconf = System.IO.File.ReadAllText("Examples/nodes.txt");
+        var nprefab = Resources.Load("Node");
+        foreach (var line in nconf.Split(new string[] { Environment.NewLine }, StringSplitOptions.None))
+        {
+            var node = (GameObject)Instantiate(nprefab);
+            node.transform.position = new Vector3(UnityEngine.Random.Range(-0.5f, 0.5f), UnityEngine.Random.Range(-0.5f, 0.5f), UnityEngine.Random.Range(-1f, 0f));
+            node.transform.localScale = new Vector3(0.02f, 0.02f, 0.02f);
+            node.transform.parent = gameObject.transform;
+            node.GetComponent<Node>().Value = line;
+            nodes.Add(line, node);
+            moveto.Add(node, node.transform.position);
         }
 
-        List<Node> links = new List<Node> ();
-        string lconf = System.IO.File.ReadAllText ("Examples/links.txt");
-        foreach (var line in lconf.Split(new string[] { Environment.NewLine }, StringSplitOptions.None)) {
-            string start = line.Split (' ') [0];
-            string end = line.Split (' ') [1];
-            Node startnode = nodes [start];
-            Node endnode = nodes [end];
+        var links = new List<Node>();
+        var lconf = System.IO.File.ReadAllText("Examples/links.txt");
+        var lprefab = Resources.Load("Link");
+        foreach (var line in lconf.Split(new string[] { Environment.NewLine }, StringSplitOptions.None))
+        {
+            var start = line.Split(' ')[0];
+            var end = line.Split(' ')[1];
+            var startnode = nodes[start];
+            var endnode = nodes[end];
 
-            GameObject o = (GameObject)Instantiate (Resources.Load ("Link"));
-            o.transform.parent = gameObject.transform;
-
-            Link link = o.GetComponent<Link> ();
-            link.node1 = startnode;
-            link.node2 = endnode;
+            var link = (GameObject)Instantiate(lprefab);
+            link.transform.parent = gameObject.transform;
+            link.GetComponent<Link>().node1 = startnode;
+            link.GetComponent<Link>().node2 = endnode;
         }
     }
 
-    void Update ()
+    void Update()
     {
         // randomly move nodes
-        List<Node> keys = new List<Node> (moveto.Keys);
-        foreach (Node node in keys) {
-            var to = moveto [node];
-            if (node.transform.position == to) {
-                moveto [node] = new Vector3 (UnityEngine.Random.Range (-0.5f, 0.5f), UnityEngine.Random.Range (-0.5f, 0.5f), UnityEngine.Random.Range (-1f, 0f));
-            } else {
-                node.transform.position = Vector3.MoveTowards (node.transform.position, to, 0.2f * Time.deltaTime);
+        var keys = new List<GameObject>(moveto.Keys);
+        foreach (var node in keys)
+        {
+            var to = moveto[node];
+            if (node.transform.position == to)
+            {
+                moveto[node] = new Vector3(UnityEngine.Random.Range(-0.5f, 0.5f), UnityEngine.Random.Range(-0.5f, 0.5f), UnityEngine.Random.Range(-1f, 0f));
             }
-        }        
+            else
+            {
+                node.transform.position = Vector3.MoveTowards(node.transform.position, to, 0.2f * Time.deltaTime);
+            }
+        }
     }
 }
