9d73f323f0272e828ec5216f79fadb5809adde2b
Francesco Cagnin <francesco.cagnin@gmail.com>
2016-05-26 09:27:57 +0200

    added different colors for different node groups

--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -1,19 +1,26 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+
+using UnityEngine;
 
 
 public class Node : MonoBehaviour
 {
+    private static readonly List<Color> colors = new List<Color>() { Color.black, Color.blue, Color.cyan, Color.gray, Color.green, Color.magenta, Color.red, Color.white, Color.yellow };
+
     public string Value
     {
-        get
-        {
-            return transform.Find("Text").GetComponent<TextMesh>().text;
-        }
         set
         {
             transform.Find("Text").GetComponent<TextMesh>().text = value;
         }
     }
 
-    public int Group { get; set; }
+    public int Group
+    { 
+        set
+        { 
+            var index = value % colors.Count;
+            transform.Find("Sphere").GetComponent<Renderer>().material.color = colors[index];
+        } 
+    }
 }
