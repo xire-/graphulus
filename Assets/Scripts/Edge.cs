0b376618aae15b453fbae8aeadf27671e9db8ff8
Francesco Cagnin <francesco.cagnin@gmail.com>
2016-05-31 11:39:16 +0200

    removed line width adjusted by distance

--- Assets/Scripts/Edge.cs
+++ Assets/Scripts/Edge.cs
@@ -14,10 +14,5 @@ public class Edge : MonoBehaviour
     {
         GetComponent<LineRenderer>().SetPosition(0, Source.transform.position);
         GetComponent<LineRenderer>().SetPosition(1, Target.transform.position);
-
-        // not perfect but enough for now
-        var startWidth = Vector3.Distance(GameObject.Find("Main Camera").transform.position, Source.transform.position) / 300f;
-        var endWidth = Vector3.Distance(GameObject.Find("Main Camera").transform.position, Target.transform.position) / 300f;
-        GetComponent<LineRenderer>().SetWidth(startWidth, endWidth);
     }
 }
