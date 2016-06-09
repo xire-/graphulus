8da052c8b73eaa66ca5c85e34bc502c68150d513
Francesco Cagnin <francesco.cagnin@gmail.com>
2016-06-09 22:19:02 +0200

    reworked light spawning

--- Assets/Scripts/Edge.cs
+++ Assets/Scripts/Edge.cs
@@ -15,38 +15,43 @@ public class Edge : MonoBehaviour
     private void LateUpdate()
     {
         Assert.IsNotNull(source);
-        GetComponent<LineRenderer>().SetPosition(0, source.transform.position);
         Assert.IsNotNull(target);
+
+        // update edge position
+        GetComponent<LineRenderer>().SetPosition(0, source.transform.position);
         GetComponent<LineRenderer>().SetPosition(1, target.transform.position);
 
-        float val = Random.value;
-        if (val < 0.0008f)
-        {
-            var light = (GameObject)Instantiate(Resources.Load("EdgeLight"));
-         
-//            var animation = transform.parent.GetComponent<World>().animationManager.Add(t => MoveLight(light, t), 1f, Easing.EaseOutQuart);
-
-            var animation = new Animation
-            {
-                duration = 1f,
-                Update = t => MoveLight(light, t),
-                Ease = Easing.EaseOutQuart
-                        
-            };
-            
-
-
-            animation.OnEnd = delegate
-            {
-                Destroy(light);
-            };
-
-            transform.parent.GetComponent<World>().animationManager.Add(animation);
-        }
+        // randomly spawn light from source node to target
+        if (Random.value < 0.0008f)
+            SpawnLight();
     }
 
-    private void MoveLight(GameObject light, float t)
+    private void SpawnLight()
     {
-        light.transform.position = Vector3.Lerp(source.transform.position, target.transform.position, t);
+        var light = (GameObject)Instantiate(Resources.Load("Light"));
+
+        // random duration and easing
+        var duration = Random.Range(0.5f, 4f);
+        System.Func<float, float>[] easings = { Easing.EaseOutCubic, Easing.EaseOutQuad, Easing.EaseOutQuart, Easing.EaseOutQuint };
+
+        // add animation
+        transform.parent.GetComponent<World>().animationManager.Add(new Animation
+        {
+            OnStart = () =>
+            {
+                light.transform.position = source.transform.position;
+                light.SetActive(true);
+            },
+            Update = t =>
+            {
+                light.transform.position = Vector3.Lerp(source.transform.position, target.transform.position, t);
+            },
+            OnEnd = () =>
+            {
+                Destroy(light);
+            },
+            duration = duration,
+            Ease = easings[Random.Range(0, easings.Length)],
+        });
     }
 }
