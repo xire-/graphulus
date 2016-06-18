e03e7d12c9f106315d8a24ee68ac6f8f13941421
Francesco Cagnin <francesco.cagnin@gmail.com>
2016-06-18 20:39:53 +0200

    added scene without VR and LP

--- Assets/Scripts/Util/CameraMovement.cs
+++ Assets/Scripts/Util/CameraMovement.cs
@@ -2,6 +2,10 @@
 
 public class CameraMovement : MonoBehaviour
 {
+    private const float _movementSpeed = 0.5f;
+    private readonly Vector2 _sensitivity = new Vector2(3, 3);
+    private readonly Vector2 _smoothing = new Vector2(3, 3);
+
     private void Awake()
     {
         Cursor.lockState = CursorLockMode.Locked;
@@ -17,29 +21,26 @@ public class CameraMovement : MonoBehaviour
     {
         // http://forum.unity3d.com/threads/a-free-simple-smooth-mouselook.73117/
 
-        Vector2 sensitivity = new Vector2(6, 6);
-        Vector2 smoothing = new Vector2(3, 3);
-
         var targetOrientation = Quaternion.Euler(transform.localRotation.eulerAngles);
 
         var mouseDelta = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
-        mouseDelta = Vector2.Scale(mouseDelta, new Vector2(sensitivity.x * smoothing.x, sensitivity.y * smoothing.y));
+        mouseDelta = Vector2.Scale(mouseDelta, new Vector2(_sensitivity.x * _smoothing.x, _sensitivity.y * _smoothing.y));
 
         Vector2 smoothMouse = new Vector2();
-        smoothMouse.x = Mathf.Lerp(smoothMouse.x, mouseDelta.x, 1f / smoothing.x);
-        smoothMouse.y = Mathf.Lerp(smoothMouse.y, mouseDelta.y, 1f / smoothing.y);
+        smoothMouse.x = Mathf.Lerp(smoothMouse.x, mouseDelta.x, 1f / _smoothing.x);
+        smoothMouse.y = Mathf.Lerp(smoothMouse.y, mouseDelta.y, 1f / _smoothing.y);
 
         Vector2 mouseAbsolute = new Vector2();
         mouseAbsolute += smoothMouse;
 
-        Vector2 clampInDegrees = new Vector2(360, 180);
-        if (clampInDegrees.x < 360)
+        Vector2 clampInDegrees = new Vector2(360f, 180f);
+        if (clampInDegrees.x < 360f)
             mouseAbsolute.x = Mathf.Clamp(mouseAbsolute.x, -clampInDegrees.x * 0.5f, clampInDegrees.x * 0.5f);
 
         var xRotation = Quaternion.AngleAxis(-mouseAbsolute.y, targetOrientation * Vector3.right);
         transform.localRotation = xRotation;
 
-        if (clampInDegrees.y < 360)
+        if (clampInDegrees.y < 360f)
             mouseAbsolute.y = Mathf.Clamp(mouseAbsolute.y, -clampInDegrees.y * 0.5f, clampInDegrees.y * 0.5f);
 
         transform.localRotation *= targetOrientation;
@@ -50,33 +51,19 @@ public class CameraMovement : MonoBehaviour
 
     private void UpdateMovement()
     {
-        const float speed = 10f;
-
         if (Input.GetKey(KeyCode.W))
-        {
-            transform.position += transform.forward * speed * Time.deltaTime;
-        }
+            transform.position += transform.forward * _movementSpeed * Time.deltaTime;
         else if (Input.GetKey(KeyCode.S))
-        {
-            transform.position -= transform.forward * speed * Time.deltaTime;
-        }
+            transform.position -= transform.forward * _movementSpeed * Time.deltaTime;
 
         if (Input.GetKey(KeyCode.A))
-        {
-            transform.position -= transform.right * speed * Time.deltaTime;
-        }
+            transform.position -= transform.right * _movementSpeed * Time.deltaTime;
         else if (Input.GetKey(KeyCode.D))
-        {
-            transform.position += transform.right * speed * Time.deltaTime;
-        }
+            transform.position += transform.right * _movementSpeed * Time.deltaTime;
 
         if (Input.GetKey(KeyCode.E))
-        {
-            transform.position += Vector3.up * speed * Time.deltaTime;
-        }
+            transform.position += Vector3.up * _movementSpeed * Time.deltaTime;
         else if (Input.GetKey(KeyCode.Q))
-        {
-            transform.position -= Vector3.up * speed * Time.deltaTime;
-        }
+            transform.position -= Vector3.up * _movementSpeed * Time.deltaTime;
     }
 }
