ece4a49cd55557c6062f261a5d29bbe8b85eaa96
Francesco Cagnin <francesco.cagnin@gmail.com>
2016-06-18 21:25:00 +0200

    renamed 'Billboard' in 'CameraFacingBillboard'

---
+++ Assets/Scripts/Util/CameraFacingBillboard.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class CameraFacingBillboard : MonoBehaviour
+{
+    private void LateUpdate()
+    {
+        // http://wiki.unity3d.com/index.php?title=CameraFacingBillboard
+
+        transform.LookAt(transform.position + Camera.main.transform.rotation * Vector3.forward,
+             Camera.main.transform.rotation * Vector3.up);
+    }
+}
