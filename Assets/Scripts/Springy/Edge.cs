9698088986cfe0d34f6ce1ff79877bc24dac99b7
KIIS <francesco.cagnin@gmail.com>
2016-06-20 16:21:33 +0200

    extracted some classes from graph

---
+++ Assets/Scripts/Springy/Edge.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Springy
+{
+    public class Edge
+    {
+        public int Id { get; private set; }
+        public Node Source { get; private set; }
+        public Node Target { get; private set; }
+        public float Length { get; set; }
+
+
+        public Edge(int id, Node source, Node target, float length)
+        {
+            this.Id = id;
+            this.Source = source;
+            this.Target = target;
+            this.Length = length;
+        }
+    }
+}
---
+++ Assets/Scripts/Springy/Node.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+namespace Springy
+{
+    public class Node : IMassPoint
+    {
+        public int Id { get; private set; }
+
+        public Vector3 Position { get; set; }
+        public Vector3 Velocity { get; set; }
+        public Vector3 Acceleration { get; set; }
+
+        public float Mass { get; set; }
+        private Vector3 forcesAccumulator { get; set; }
+
+        float IMassPoint.Mass
+        {
+            get { return Mass; }
+            set { Mass = value; }
+        }
+
+        Vector3 IMassPoint.Position
+        {
+            get { return Position; }
+            set { Position = value; }
+        }
+
+        public Node(int id, float mass, float randRange = 100f)
+        {
+            this.Id = id;
+
+            this.Position = new Vector3(
+                UnityEngine.Random.Range(-randRange, randRange),
+                UnityEngine.Random.Range(-randRange, randRange),
+                UnityEngine.Random.Range(-randRange, randRange)
+                );
+            this.Velocity = new Vector3();
+            this.Acceleration = new Vector3();
+
+            this.Mass = mass;
+            this.forcesAccumulator = new Vector3();
+        }
+
+        public void AddForce(Vector3 f)
+        {
+            forcesAccumulator += f;
+        }
+
+        public void ComputeAcceleration()
+        {
+            Acceleration = forcesAccumulator / Mass;
+            forcesAccumulator = Vector3.zero;
+        }
+    }
+}
