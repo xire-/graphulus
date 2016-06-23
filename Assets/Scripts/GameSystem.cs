69c5a8095c15cbc2103f17b92153f3cb9be56367
KIIS <francesco.cagnin@gmail.com>
2016-06-23 11:14:01 +0200

    added node selected color as theme field

--- Assets/Scripts/GameSystem.cs
+++ Assets/Scripts/GameSystem.cs
@@ -159,6 +159,7 @@ public class GameSystem : MonoBehaviour {
                 name = "Dark",
                 skyboxColor = new Color32(0x10, 0x0F, 0x0F, 0xFF),
                 nodeColor = new Color32(0x17, 0xE2, 0xDA, 0xA1),
+                nodeSelectedColor = new Color32(0xD7, 0x8F, 0x32, 0xD9),
                 textColor = new Color32(0xE3, 0xE3, 0xE3, 0xFF),
                 edgeColor = new Color32(0xF3, 0xF3, 0xF3, 0x64)
             },
@@ -166,6 +167,7 @@ public class GameSystem : MonoBehaviour {
                 name = "Light",
                 skyboxColor = new Color32(0x02, 0x44, 0x5F, 0xFF),
                 nodeColor = new Color32(0x10, 0xAA, 0x51, 0xD2),
+                nodeSelectedColor = new Color32(0xE4, 0xE6, 0x2D, 0xD4),
                 textColor = new Color32(0x9E, 0xCC, 0xC7, 0xFF),
                 edgeColor = new Color32(0xD9, 0x68, 0x3E, 0xC6)
             }
--- Assets/Scripts/Util/Theme.cs
+++ Assets/Scripts/Util/Theme.cs
@@ -4,6 +4,7 @@ public struct Theme {
     public Color edgeColor;
     public string name;
     public Color nodeColor;
+    public Color nodeSelectedColor;
     public Color skyboxColor;
     public Color textColor;
 }
