511efe56fd9617ff36b5ea9203d6d8c9ad909014
KIIS <francesco.cagnin@gmail.com>
2016-06-24 14:26:23 +0200

    added button to reset graph transform

--- Assets/Scripts/Util/Menu.cs
+++ Assets/Scripts/Util/Menu.cs
@@ -25,6 +25,45 @@ public class Menu : MonoBehaviour {
         GameSystem.Instance.EdgesActive = _panelTransform.Find("ToggleTextsActive").GetComponent<Toggle>().isOn;
     }
 
+    public void ResetGraphTransform() {
+        var pinchController = GameObject.Find("PinchController");
+        if (pinchController != null) {
+            var pinchControllerInitialPosition = pinchController.transform.position;
+            var pinchControllerInitialScale = pinchController.transform.localScale;
+            var graphInitialPosition = GameSystem.Instance.graph.transform.position;
+            var graphInitialScale = GameSystem.Instance.graph.transform.localScale;
+            var graphInitialRotation = GameSystem.Instance.graph.transform.rotation;
+
+            GameSystem.Instance.Animate(new Animation {
+                Update = t => {
+                    pinchController.transform.position = Vector3.Lerp(pinchControllerInitialPosition, Vector3.zero, t);
+                    pinchController.transform.localScale = Vector3.Lerp(pinchControllerInitialScale, Vector3.one, t);
+
+                    GameSystem.Instance.graph.transform.position = Vector3.Lerp(graphInitialPosition, Vector3.zero, t);
+                    GameSystem.Instance.graph.transform.localScale = Vector3.Lerp(graphInitialScale, Vector3.one, t);
+                    GameSystem.Instance.graph.transform.rotation = Quaternion.Lerp(graphInitialRotation, Quaternion.Euler(0f, 0f, 0f), t);
+                },
+                duration = 2f,
+                Ease = Easing.EaseOutQuart,
+            });
+        }
+        else {
+            var graphInitialPosition = GameSystem.Instance.graph.transform.position;
+            var graphInitialScale = GameSystem.Instance.graph.transform.localScale;
+            var graphInitialRotation = GameSystem.Instance.graph.transform.rotation;
+
+            GameSystem.Instance.Animate(new Animation {
+                Update = t => {
+                    GameSystem.Instance.graph.transform.position = Vector3.Lerp(graphInitialPosition, Vector3.zero, t);
+                    GameSystem.Instance.graph.transform.localScale = Vector3.Lerp(graphInitialScale, Vector3.one, t);
+                    GameSystem.Instance.graph.transform.rotation = Quaternion.Lerp(graphInitialRotation, Quaternion.Euler(0f, 0f, 0f), t);
+                },
+                duration = 2f,
+                Ease = Easing.EaseOutQuart,
+            });
+        }
+    }
+
     public void SwitchTheme() {
         GameSystem.Instance.SwitchTheme();
     }
