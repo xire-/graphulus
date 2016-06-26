49c3f2d7b1fc3fd96562ffc2939a0bd864b070d1
Francesco Cagnin <francesco.cagnin@gmail.com>
2016-06-26 17:36:15 +0200

    extracted Settings class into its own file

---
+++ Assets/Scripts/Util/Settings.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class Settings {
+
+    public static readonly Theme[] themes = new Theme[] {
+            new Theme {
+                name = "Dark",
+                skyboxColor = new Color32(0x10, 0x0F, 0x0F, 0xFF),
+                nodeColor = new Color32(0x17, 0xE2, 0xDA, 0xA1),
+                nodeSelectedColor = new Color32(0xD7, 0x8F, 0x32, 0xD9),
+                labelColor = new Color32(0xE3, 0xE3, 0xE3, 0xFF),
+                edgeColor = new Color32(0xF3, 0xF3, 0xF3, 0x64)
+            },
+            new Theme {
+                name = "Light",
+                skyboxColor = new Color32(0x02, 0x44, 0x5F, 0xFF),
+                nodeColor = new Color32(0x10, 0xAA, 0x51, 0xD2),
+                nodeSelectedColor = new Color32(0xE4, 0xE6, 0x2D, 0xD4),
+                labelColor = new Color32(0x9E, 0xCC, 0xC7, 0xFF),
+                edgeColor = new Color32(0xD9, 0x68, 0x3E, 0xC6)
+            }
+        };
+
+    public bool autoRotationEnabled = false;
+    public float autoRotationSpeed = 15f;
+    public bool edgesActive = true;
+    public string graph = "Miserables";
+    public bool labelsActive = true;
+    public int themeIndex = 0;
+}
