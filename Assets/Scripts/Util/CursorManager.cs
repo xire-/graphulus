b34109588da57b964f2bdcf719354278dceefb2c
Francesco Cagnin <francesco.cagnin@gmail.com>
2016-06-26 12:52:26 +0200

    doubled cursor size and fixed its color to white

--- Assets/Scripts/Util/CursorManager.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using UnityEngine.UI;
-
-public class CursorManager : MonoBehaviour {
-
-    private void OnEnable() {
-        GetComponent<Image>().color = GameSystem.Instance.Theme.nodeColor;
-    }
-}
